c4ecad0cd4a6de371fd0b4f529d550e5ee224a02
Sandy Goss <alexander.e.goss.18@dartmouth.edu>
2018-07-10 23:17:20 -0400

    First graph request

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import FBSDK, { LoginButton } from 'react-native-fbsdk';
+import FBSDK,
+{
+  LoginButton,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager,
+} from 'react-native-fbsdk';
 
 class Login extends React.Component {
+  onLoginSuccess = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      const accessToken = data.accessToken;
+      // alert(accessToken.toString());
+
+      const responseInfoCallback = (error, result) => {
+        if (error) {
+          console.log(error);
+          alert(`Error fetching data: ${error.toString()}`);
+        } else {
+          console.log(result);
+          alert(`Success fetching data: ${result.toString()}`);
+        }
+      };
+
+      const infoRequest = new GraphRequest(
+        '/me',
+        {
+          accessToken,
+          parameters: {
+            fields: {
+              string: 'email,name,first_name,middle_name,last_name',
+            },
+          },
+        },
+        responseInfoCallback,
+      );
+
+      // Start the graph request.
+      new GraphRequestManager().addRequest(infoRequest).start();
+    });
+  }
+
   render() {
     return (
       <View>
         <LoginButton
           readPermission={['public_profile']}
           onLoginFinished={
-          (error, result) => {
-            if (error) {
-              alert(`Login failed with error: ${result.error}`);
-            } else if (result.isCancelled) {
-              alert('Login was cancelled');
-            } else {
-              alert(`Login was successful with permissions: ${result.grantedPermissions}`);
+            (error, result) => {
+              if (error) {
+                alert(`Login failed with error: ${result.error}`);
+              } else if (result.isCancelled) {
+                alert('Login was cancelled');
+              } else {
+                // alert(`Login was successful with permissions: ${result.grantedPermissions}`);
+                this.onLoginSuccess();
+              }
             }
           }
-        }
-          onLogoutFinished={() => alert('User logged out')}
+          // onLogoutFinished={() => alert('User logged out')}
         />
       </View>
     );
